30352e8f308d3df88a672470926d6a1a89eb0d45
Hollis Lau <hollislau@hotmail.com>
2016-04-17 17:55:28 -0700

    Add default response for root and 404; add framework for remaining REST functions

--- bdh.js
+++ bdh.js
@@ -2,13 +2,19 @@ const bdh = require(__dirname + "/big-damn-heroes");
 
 bdh.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.send("You can't take the sky from me.");
+  res.send("I'm a leaf on the wind, watch how I soar.");
   res.end();
 });
 
 bdh.get("/serenity", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.send("<h1>You can't stop the signal!</h1>");
+  res.end();
+});
+
+bdh.badRoute((req, res) => {
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.send("Best be on your merry.");
   res.end();
 });
 
